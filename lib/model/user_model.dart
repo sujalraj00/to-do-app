79a100c9377af3d8e932ef6b4ddc889657dc81cb
Sujal Raj <sujalraj611@gmail.com>
2024-12-19 15:32:45 +0530

    user model added

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -8,45 +8,80 @@ class UserModel {
   final DateTime createdAt;
   final DateTime updatedAt;
 
-  UserModel({
-    required this.id,
-    required this.name,
-    required this.email,
-    required this.token,
-    required this.createdAt,
-    required this.updatedAt
-  });
+  UserModel(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.token,
+      required this.createdAt,
+      required this.updatedAt});
 
-  UserModel copyWith({
-    String? id,
-    String? name,
-    String? email,
-    String? token,
-    DateTime? createdAt,
-    DateTime? updatedAt
-  }){
+  UserModel copyWith(
+      {String? id,
+      String? name,
+      String? email,
+      String? token,
+      DateTime? createdAt,
+      DateTime? updatedAt}) {
     return UserModel(
-      id: id ?? this.id, 
-      name: name ?? this.name, 
-      email: email ?? this.email, 
-      token: token ?? this.token, 
-      createdAt: createdAt ?? this.createdAt, 
-      updatedAt: updatedAt ?? this.updatedAt
-      );
+        id: id ?? this.id,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        token: token ?? this.token,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt);
   }
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic> {
-      'id' : id,
-      'email' : email,
-      'name' : name,
-      'token' : token,
-      'createdAt' : createdAt.millisecondsSinceEpoch,
-      'updatedAt' : updatedAt.millisecondsSinceEpoch
+    return <String, dynamic>{
+      'id': id,
+      'email': email,
+      'name': name,
+      'token': token,
+      'createdAt': createdAt.millisecondsSinceEpoch,
+      'updatedAt': updatedAt.millisecondsSinceEpoch
     };
   }
 
-  factory UserModel.fromMap(Map<String, dynamic> map){
-    return UserModel(id: id, name: name, email: email, token: token, createdAt: createdAt, updatedAt: updatedAt)
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+        id: map['id'] ?? '',
+        name: map['name'] ?? '',
+        email: map['email'] ?? '',
+        token: map['token'] ?? '',
+        createdAt: DateTime.parse(map['createdAt']),
+        updatedAt: DateTime.parse(map['updatedAt']));
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserModel(id: $id, name: $name, email: $email, token: $token, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  @override
+  bool operator ==(covariant UserModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.email == email &&
+        other.token == token &&
+        other.createdAt == createdAt &&
+        other.updatedAt == updatedAt;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        email.hashCode ^
+        token.hashCode ^
+        createdAt.hashCode ^
+        updatedAt.hashCode;
   }
 }
