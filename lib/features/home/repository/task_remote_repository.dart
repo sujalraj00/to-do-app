237ccd2ffa01f74ceab7b20900eb6b68b44f3188
Sujal Raj <sujalraj611@gmail.com>
2024-12-28 19:21:25 +0530

    task remote repo added

---
+++ lib/features/home/repository/task_remote_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:task_app/core/constants/constants.dart';
+import 'package:task_app/model/task_model.dart';
+
+class TaskRemoteRepository {
+  Future<TaskModel> createTask(
+      {required String title,
+      required String description,
+      required String hexColor,
+      required String token,
+      required DateTime dueAt}) async {
+    try {
+      final res = await http.post(Uri.parse("${Constants.backendUri}/tasks"),
+          headers: {'Content-Type': 'application/json', 'x-auth-token': token},
+          body: jsonEncode({
+            "title": title,
+            "description": description,
+            "hexColor": hexColor,
+            "dueAt": dueAt.toIso8601String()
+          }));
+
+      if (res.statusCode != 201) {
+        throw jsonDecode(res.body)['error'];
+      }
+
+      return TaskModel.fromJson(res.body);
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
